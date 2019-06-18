84cdf4cbbb37da68fe69839e48b10aac7f55ed6b
vinodtk <vinod@appzui.com>
2019-06-18 18:18:56 +0530

    Add project files.

---
+++ ReactApp3/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactApp3.Models;
+
+namespace ReactApp3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is another comment"
+                },
+            };
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ReactApp3/wwwroot/js/tutorial.jsx
@@ -0,0 +1,76 @@
+﻿class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+    }
+
+    loadCommentsFromServer() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+        };
+        xhr.send();
+        this.setState({ data: data });
+        console.log(this.state.data);
+
+    }
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        window.setInterval(
+            () => this.loadCommentsFromServer(),
+            this.props.pollInterval,
+        );
+    }
+
+    render() {
+        
+        return (
+  
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+class CommentList extends React.Component {
+   
+    render() {
+      
+        const commentNodes = this.props.data.map( comment => (
+            <Comment author={comment.Author} key={comment.Id}>
+                {comment.Text}
+            </Comment>
+        ));
+        return <div className="commentList">{commentNodes}</div>;
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">Hello, world! I am a CommentForm.</div>
+        );
+    }
+}
+
+class Comment extends React.Component {
+    render() {
+       const md = new Remarkable()
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <CommentBox url="/comments" pollInterval={2000} />,
+    document.getElementById('content'),
+);
